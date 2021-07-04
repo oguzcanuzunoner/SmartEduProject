79ed5f605abbfc1d893a80fcdfe081fa518238a1
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-07-04 13:18:19 +0300

    Connect MongoDb and Add Courses Page

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const pageRoute = require("./routers/pageRoute");
+const courseRoute = require("./routers/courseRoute");
 const app = express();
+
+//Connect DB
+mongoose
+  .connect("mongodb://localhost/smartedu-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB Connected Successfuly");
+  });
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //Routers
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
