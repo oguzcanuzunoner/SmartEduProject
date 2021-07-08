3c50c0a9825437448ffa173d82e0f667a9618732
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-07-08 11:05:29 +0300

    add searchbar

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -23,6 +23,9 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+
+    const query = req.query
+
     const category = await Category.findOne({ slug: categorySlug });
 
     let filter = {};
@@ -31,7 +34,19 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort("-createdAt");
+    if (query){
+      filter= {name : query}
+    }
+
+    if (!query && !categorySlug){
+      filter.name= "",
+      filter.category = null
+    }
+    
+
+    const courses = await Course.find({
+      $or : [{name:{$regex: '.*'+filter.name+ '.*',$options:'i'}},{category:filter.category}]
+    }).sort("-createdAt").populate('user');
     const categories = await Category.find();
 
     res.status(200).render("courses", {
@@ -53,11 +68,13 @@ exports.getCourse = async (req, res) => {
     const course = await Course.findOne({ slug: req.params.slug }).populate(
       "user"
     );
+    const categories = await Category.find();
 
     res.status(200).render("course", {
       course,
       page_name: "courses",
-      user
+      user,
+      categories
     });
   } catch (error) {
     res.status(400).json({
