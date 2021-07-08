2d8aa2352ea022e8592262d1bb8fa77de971005f
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-07-08 15:37:57 +0300

    edit home page, add categories add-delete buttons

--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -5,5 +5,6 @@ const categoryController = require("../controllers/categoryController");
 const router = express.Router();
 
 router.route("/").post(categoryController.createCategory); //http://localhost:3000/categories
+router.route("/:id").delete(categoryController.deleteCategory); //http://localhost:3000/categories/:id
 
 module.exports = router;
