2443cbe3e7c86a6bcf85784098f512bc47e1252a
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-07-08 15:13:33 +0300

    add admin panel

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -27,4 +27,5 @@ router.route("/signup").post(
 router.route("/login").post(authController.loginUser); //http://localhost:3000/users/login
 router.route("/logout").get(authController.logoutUser); //http://localhost:3000/users/logout
 router.route("/dashboard").get(authMiddleware,authController.getDashboardPage); //http://localhost:3000/users/dashboard
+router.route("/:id").delete(authController.deleteUser); //http://localhost:3000/users/:id
 module.exports = router;
