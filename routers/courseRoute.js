1e8808fc07fa2a1825e7cb6228541b859fd77d69
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-07-08 03:49:47 +0300

    add course - student relationship, student can add course

--- routers/courseRoute.js
+++ routers/courseRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
 const courseController = require("../controllers/courseController");
-const roleMiddleware = require('../middlewares/roleMiddleware')
+const roleMiddleware = require("../middlewares/roleMiddleware");
 const router = express.Router();
 
-router.route("/").post(roleMiddleware(["teacher","admin"]),courseController.createCourse); //http://localhost:3000/courses
-router.route("/").get(courseController.getAllCourses)
-router.route("/:slug").get(courseController.getCourse)
+router
+  .route("/")
+  .post(roleMiddleware(["teacher", "admin"]), courseController.createCourse); //http://localhost:3000/courses
+router.route("/").get(courseController.getAllCourses);
+router.route("/:slug").get(courseController.getCourse);
+router.route("/enroll").post(courseController.enrollCourse);
 module.exports = router;
